709451c4c2c9cffce1e24188e234c38d1b4a0370
chethmiYWD <chethmi.20230809@iit.ac.lk>
2025-03-06 11:21:45 +0530

    Update App.js

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import TodoList from './TodoList';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [tasks, setTasks] = useState([]);
+
+    const addTask = (task) => {
+        setTasks([...tasks, task]);
+    };
+
+    const updateTask = (index, updatedTask) => {
+        const newTasks = tasks.map((task, i) => (i === index ? updatedTask : task));
+        setTasks(newTasks);
+    };
+
+    const deleteTask = (index) => {
+        const newTasks = tasks.filter((_, i) => i !== index);
+        setTasks(newTasks);
+    };
+
+    return (
+        <div className="app-container">
+            <h2>React To-Do List</h2>
+            <TodoList tasks={tasks} addTask={addTask} updateTask={updateTask} deleteTask={deleteTask} />
+        </div>
+    );
 }
 
 export default App;
